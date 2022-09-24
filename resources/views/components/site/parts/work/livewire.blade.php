8b6d2dbabb35d23d2a71b3c0d4cc29c699f0df24
sato398 <shonen5359@gmail.com>
2022-09-25 01:33:50 +0900

    fix

--- resources/views/components/site/parts/work/livewire.blade.php
+++ resources/views/components/site/parts/work/livewire.blade.php
@@ -1,7 +1,12 @@
 <section id="portfolio" class="content-section">
     <div class="container">
-        @foreach ($workCategories as $workCategory)
-            <x-site.parts.work.work_list :workCategory="$workCategory"/>
-        @endforeach
+        @if ($workItems->isEmpty())
+            <p class="text-center mt-5">実績はありません。</p>
+        @else
+            @foreach ($workCategories as $workCategory)
+                <x-site.parts.work.work_list :workCategory="$workCategory"/>
+            @endforeach
+        @endif
+
     </div>
 </section>
--- resources/views/components/site/parts/work/work_list.blade.php
+++ resources/views/components/site/parts/work/work_list.blade.php
@@ -2,14 +2,17 @@
     'workCategory' => $workCategory ?? '',
 ])
 
-<div class="portfolio-item-wrap">
-    <div class="portfolio-item-head-wrap">
-        <h6 class="text-center text-danger mb-0">{{ $workCategory->name }}</h6>
-        <h1 class="text-center portfolio-item-head">{{ $workCategory->name_en }}</h1>
+@if (!$workCategory->works->where('work_category_id', $workCategory->id)->isEmpty())
+    <div class="portfolio-item-wrap">
+        <div class="portfolio-item-head-wrap">
+            <h6 class="text-center text-danger mb-0">{{ $workCategory->name }}</h6>
+            <h1 class="text-center portfolio-item-head">{{ $workCategory->name_en }}</h1>
+        </div>
+        <div class="row g-0 mt-3">
+            @foreach ($workCategory->works->where('work_category_id', $workCategory->id) as $item)
+                <x-site.parts.work.item :item="$item" />
+            @endforeach
+        </div>
     </div>
-    <div class="row g-0 mt-3">
-        @foreach ($workCategory->works->where('work_category_id', $workCategory->id) as $item)
-            <x-site.parts.work.item :item="$item" />
-        @endforeach
-    </div>
-</div>
+@endif
+
